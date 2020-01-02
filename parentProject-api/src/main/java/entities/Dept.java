62d4af9fcdeb11280c0f32935aaae059fbcf453f
zhouhao66 <59349628+zhouhao66@users.noreply.github.com>
2020-01-02 17:20:58 +0800

    Create Dept.java

---
+++ parentProject-api/src/main/java/entities/Dept.java
@@ -0,0 +1,53 @@
+package entities;
+
+import java.io.Serializable;
+
+public class Dept implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private long deptno;
+	private String dname;
+	private String db_source;
+
+	public Dept() {
+		super();
+	}
+
+	public Dept(long deptno, String dname, String db_source) {
+		super();
+		this.deptno = deptno;
+		this.dname = dname;
+		this.db_source = db_source;
+	}
+
+	public long getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(long deptno) {
+		this.deptno = deptno;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getDb_source() {
+		return db_source;
+	}
+
+	public void setDb_source(String db_source) {
+		this.db_source = db_source;
+	}
+
+	@Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", db_source=" + db_source + "]";
+	}
+
+}
